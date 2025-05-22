0023753f51ed5cbc30ff1a7f62f7bfeec5e8842a
chajus1 <chajus1@googlemail.com>
2025-05-22 04:01:28 +0100

    populated PaintballGun Class

--- PaintballGunConsole/PaintballGun.cs
+++ PaintballGunConsole/PaintballGun.cs
@@ -10,6 +10,29 @@ namespace PaintballGunConsole
     {
         public const int MAGAZINE_SIZE = 16;
 
-        private
+        private int balls;
+        private int ballsLoaded;
+
+        public int GetBallsLoaded() { return ballsLoaded; }
+
+        public bool IsEmpty() { return ballsLoaded == 0; }
+
+        public int GetBalls() { return balls; }
+
+        public void SetBalls(int numberOfBalls)
+        {
+            if (numberOfBalls > 0)
+                balls = numberOfBalls;
+            else
+                ballsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (ballsLoaded == 0) return false;
+            ballsLoaded--;
+            balls--;
+            return true;
+        }
     }
 }
